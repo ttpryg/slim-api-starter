448c8e199555c6b794ae1d1d6872cdf221a6cdd6
Toto <totoprayogo1916@gmail.com>
2026-02-11 22:33:40 +0700

    ttpryg/test (#1)
    * feat: Test
    
    * feat: Add .env support with vlucas/phpdotenv and use APP_ENV for displayErrorDetails
    
    * feat: Add GitHub Actions for running PHPUnit tests
    
    * up
    
    * up

--- public/index.php
+++ public/index.php
@@ -2,41 +2,7 @@
 
 declare(strict_types=1);
 
-use DI\ContainerBuilder;
-use Slim\Factory\AppFactory;
-
-require __DIR__ . '/../vendor/autoload.php';
-
-// Instantiate PHP-DI ContainerBuilder
-$containerBuilder = new ContainerBuilder();
-
-// Set up settings
-$settings = require __DIR__ . '/../config/settings.php';
-$containerBuilder->addDefinitions($settings);
-
-// Set up dependencies
-$dependencies = require __DIR__ . '/../config/dependencies.php';
-$dependencies($containerBuilder);
-
-// Build PHP-DI Container instance
-$container = $containerBuilder->build();
-
-// Instantiate the app
-AppFactory::setContainer($container);
-$app = AppFactory::create();
-
-// Register routes
-$routes = require __DIR__ . '/../config/routes.php';
-$routes($app);
-
-// Add Routing Middleware
-$app->addRoutingMiddleware();
-
-// Add Error Middleware
-$errorMiddleware = $app->addErrorMiddleware(
-    $container->get('settings')['displayErrorDetails'],
-    $container->get('settings')['logError'],
-    $container->get('settings')['logErrorDetails']
-);
+/** @var \Slim\App $app */
+$app = require __DIR__ . '/../config/bootstrap.php';
 
 $app->run();
---
+++ tests/TestCase.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Slim\App;
+use Slim\Psr7\Factory\ServerRequestFactory;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+abstract class TestCase extends PHPUnitTestCase
+{
+    protected function getAppInstance(): App
+    {
+        return require __DIR__ . '/../config/bootstrap.php';
+    }
+
+    protected function createRequest(
+        string $method,
+        string $path,
+        array $headers = ['HTTP_ACCEPT' => 'application/json'],
+        array $cookies = [],
+        array $serverParams = []
+    ): Request {
+        $factory = new ServerRequestFactory();
+        $request = $factory->createServerRequest($method, $path, $serverParams);
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request->withCookieParams($cookies);
+    }
+}
