95384a18db530d504b3849cc243c72d62feda1b7
Toto <totoprayogo1916@gmail.com>
2026-04-22 20:24:24 +0700

    feat: add illuminate/database and illuminate/events dependencies and configure PHPUnit test script (#2)

--- config/dependencies.php
+++ config/dependencies.php
@@ -3,9 +3,46 @@
 declare(strict_types=1);
 
 use DI\ContainerBuilder;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        // Define your dependencies here
+        Capsule::class => function () {
+            $dbDriver = getenv('DB_DRIVER') ?: 'mysql';
+            $dbHost = getenv('DB_HOST') ?: 'localhost';
+            $dbPort = getenv('DB_PORT') ?: 3306;
+            $dbDatabase = getenv('DB_DATABASE') ?: 'slim_api';
+            $dbUsername = getenv('DB_USERNAME') ?: 'root';
+            $dbPassword = getenv('DB_PASSWORD') ?: '';
+            $dbCharset = getenv('DB_CHARSET') ?: 'utf8mb4';
+            $dbCollation = getenv('DB_COLLATION') ?: 'utf8mb4_unicode_ci';
+            $dbPrefix = getenv('DB_PREFIX') ?: '';
+
+            $config = [
+                'driver'    => $dbDriver,
+                'host'      => $dbHost,
+                'port'      => $dbPort,
+                'database'  => $dbDatabase,
+                'username'  => $dbUsername,
+                'password'  => $dbPassword,
+                'charset'   => $dbCharset,
+                'collation'  => $dbCollation,
+                'prefix'     => $dbPrefix,
+            ];
+
+            $capsule = new Capsule(new Container());
+
+            $capsule->addConnection($config);
+
+            $capsule->setEventDispatcher(new Dispatcher(new Container()));
+
+            $capsule->setAsGlobal();
+
+            $capsule->bootEloquent();
+
+            return $capsule;
+        },
     ]);
 };
---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+putenv('APP_ENV=testing');
+putenv('APP_DEBUG=true');
+putenv('DB_DRIVER=sqlite');
+putenv('DB_DATABASE=:memory:');
+
+$_ENV['APP_ENV'] = 'testing';
+$_ENV['APP_DEBUG'] = true;
+$_ENV['DB_DRIVER'] = 'sqlite';
+$_ENV['DB_DATABASE'] = ':memory:';
