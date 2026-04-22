ce37981ebe0f714a83fafc401bc2e7af797cc8ab
Toto <totoprayogo1916@gmail.com>
2026-04-22 23:10:47 +0700

    feat: add ResponseTrait and integrate into HomeAction and MakeActionCommand for standardized JSON responses (#11)

--- tests/Action/HomeActionTest.php
+++ tests/Action/HomeActionTest.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace App\Test\Action;
 
 use App\Test\TestCase;
+use App\Traits\ResponseTrait;
 
 class HomeActionTest extends TestCase
 {
+    use ResponseTrait;
+
     public function test_home_action_returns_hello_world(): void
     {
         $app = $this->getAppInstance();
@@ -15,7 +18,13 @@ class HomeActionTest extends TestCase
         $response = $app->handle($request);
 
         $payload = (string) $response->getBody();
-        $expectedPayload = json_encode(['message' => 'Hello World!']);
+
+        $expectedResponse = $this->success(
+            $app->getResponseFactory()->createResponse(),
+            null,
+            'Hello World!'
+        );
+        $expectedPayload = (string) $expectedResponse->getBody();
 
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals('application/json', $response->getHeaderLine('Content-Type'));
