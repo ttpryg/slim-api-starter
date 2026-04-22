33563b796229642e186ff5fa7143fae36fa31ef3
Toto <totoprayogo1916@gmail.com>
2026-04-22 20:32:35 +0700

    refactor: decouple database configuration from Capsule definition by injecting settings from container

--- config/dependencies.php
+++ config/dependencies.php
@@ -6,35 +6,14 @@ use DI\ContainerBuilder;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
+use Psr\Container\ContainerInterface;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        Capsule::class => function () {
-            $dbDriver = getenv('DB_DRIVER') ?: 'mysql';
-            $dbHost = getenv('DB_HOST') ?: 'localhost';
-            $dbPort = getenv('DB_PORT') ?: 3306;
-            $dbDatabase = getenv('DB_DATABASE') ?: 'slim_api';
-            $dbUsername = getenv('DB_USERNAME') ?: 'root';
-            $dbPassword = getenv('DB_PASSWORD') ?: '';
-            $dbCharset = getenv('DB_CHARSET') ?: 'utf8mb4';
-            $dbCollation = getenv('DB_COLLATION') ?: 'utf8mb4_unicode_ci';
-            $dbPrefix = getenv('DB_PREFIX') ?: '';
-
-            $config = [
-                'driver'    => $dbDriver,
-                'host'      => $dbHost,
-                'port'      => $dbPort,
-                'database'  => $dbDatabase,
-                'username'  => $dbUsername,
-                'password'  => $dbPassword,
-                'charset'   => $dbCharset,
-                'collation'  => $dbCollation,
-                'prefix'     => $dbPrefix,
-            ];
-
+        Capsule::class => function (ContainerInterface $c) {
             $capsule = new Capsule(new Container());
 
-            $capsule->addConnection($config);
+            $capsule->addConnection($c->get('database'));
 
             $capsule->setEventDispatcher(new Dispatcher(new Container()));
 
