f8c121a6d9f7c54cc01b1fc8e81de9a56f5810ff
Toto <totoprayogo1916@gmail.com>
2026-04-22 20:19:23 +0700

    feat: add illuminate/database and illuminate/events dependencies and configure PHPUnit test script

--- config/settings.php
+++ config/settings.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 return [
     'settings' => [
-        'displayErrorDetails' => (getenv('APP_ENV') === 'development' || filter_var(getenv('APP_DEBUG'), FILTER_VALIDATE_BOOLEAN)), // Should be false in production
+        'displayErrorDetails' => (getenv('APP_ENV') === 'development' || filter_var(getenv('APP_DEBUG'), FILTER_VALIDATE_BOOLEAN)),
         'logError'            => false,
         'logErrorDetails'     => false,
+    ],
+
+    'database' => [
+        'driver'    => getenv('DB_DRIVER') ?: 'mysql',
+        'host'      => getenv('DB_HOST') ?: 'localhost',
+        'port'      => getenv('DB_PORT') ?: 3306,
+        'database'  => getenv('DB_DATABASE') ?: 'slim_api',
+        'username'  => getenv('DB_USERNAME') ?: 'root',
+        'password'  => getenv('DB_PASSWORD') ?: '',
+        'charset'   => getenv('DB_CHARSET') ?: 'utf8mb4',
+        'collation' => getenv('DB_COLLATION') ?: 'utf8mb4_unicode_ci',
+        'prefix'    => getenv('DB_PREFIX') ?: '',
+        'url'       => getenv('DB_URL') ?: null,
     ],
 ];
