5d467a608a6d573f85d0e3fe960078619c88401d
Toto <totoprayogo1916@gmail.com>
2026-05-14 17:48:42 +0700

    feat: configure custom JSON error handler

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 use DI\ContainerBuilder;
 use Dotenv\Dotenv;
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Log\LoggerInterface;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpMethodNotAllowedException;
+use Slim\Exception\HttpNotFoundException;
 use Slim\Factory\AppFactory;
 
 require __DIR__.'/../vendor/autoload.php';
@@ -50,5 +54,45 @@ $errorMiddleware = $app->addErrorMiddleware(
     $container->get('settings')['logErrorDetails'],
     $container->get(LoggerInterface::class)
 );
+
+$errorMiddleware->setDefaultErrorHandler(function (
+    ServerRequestInterface $request,
+    Throwable $exception,
+    bool $displayErrorDetails,
+    bool $logErrors,
+    bool $logErrorDetails
+) use ($app) {
+    $response = $app->getResponseFactory()->createResponse();
+
+    $status = 500;
+    if ($exception instanceof HttpNotFoundException) {
+        $status = 404;
+    } elseif ($exception instanceof HttpMethodNotAllowedException) {
+        $status = 405;
+    } elseif ($exception instanceof HttpBadRequestException) {
+        $status = 400;
+    }
+
+    $payload = [
+        'success' => false,
+        'message' => $exception->getMessage() ?: 'Internal Server Error',
+    ];
+
+    if ($displayErrorDetails) {
+        $payload['errors'] = [
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+            'trace' => explode("\n", $exception->getTraceAsString()),
+        ];
+    }
+
+    $response->getBody()->write(
+        json_encode($payload, JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES)
+    );
+
+    return $response
+        ->withHeader('Content-Type', 'application/json')
+        ->withStatus($status);
+});
 
 return $app;
